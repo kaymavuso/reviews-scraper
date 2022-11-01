8bd77355f104f60c9ee214596ada9cc97ac2e790
kaymavuso <kaymavuso@gmail.com>
2022-11-01 18:21:28 +0000

    CWe have aDjango project named reviews-scraper , ith initiall templates

--- reviews-scraper/urls.py
+++ reviews-scraper/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('jobs.urls')), # creating a connection with the jobs app to include all the urls from the jobs app directory
 ]
